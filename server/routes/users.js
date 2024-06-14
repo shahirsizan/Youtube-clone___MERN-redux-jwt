53cb1510e009936ef96f9c7870fc429500048dfa
shahirsizan <shahir.sizan18@gmail.com>
2024-06-15 00:36:32 +0600

    Added controller code for `update` and `deleteUser`

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,37 @@
 import express from "express";
-import { test } from "../controllers/user.js";
+import {
+	update,
+	deleteUser,
+	getUser,
+	subscribe,
+	unsubscribe,
+	like,
+	dislike,
+} from "../controllers/user.js";
 // don't write "../controllers/user", this will render `ERR_MODULE_NOT_FOUND`
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
+
+//update user
+router.put("/:id", verifyToken, update);
+
+//delete user
+router.delete("/:id", verifyToken, deleteUser);
+
+//get a user
+router.get("/find/:id", getUser);
+
+//subscribe a user
+router.put("/sub/:id", verifyToken, subscribe);
+
+//unsubscribe a user
+router.put("/unsub/:id", verifyToken, unsubscribe);
+
+//like a video
+router.put("/like/:videoId", verifyToken, like);
+
+//dislike a video
+router.put("/dislike/:videoId", verifyToken, dislike);
 
 export default router;
