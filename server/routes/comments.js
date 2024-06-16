307908c12d3dbe38825c8f7089c957392158ed98
shahirsizan <shahir.sizan18@gmail.com>
2024-06-16 22:46:08 +0600

    Added more Comment controllers. Sufficient APIs are added to get started with React library.

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,7 +1,11 @@
 import express from "express";
 import {} from "../controllers/comment.js";
 // don't write "../controllers/user", this will render `ERR_MODULE_NOT_FOUND`
-// import { addComment, deleteComment, getComments } from "../controllers/comment.js";
+import {
+	addComment,
+	deleteComment,
+	getComments,
+} from "../controllers/comment.js";
 import { verifyToken } from "../verifyToken.js";
 const router = express.Router();
 
