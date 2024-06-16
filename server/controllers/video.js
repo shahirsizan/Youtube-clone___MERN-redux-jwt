ad8a71996edf4d24915cb28ac969c53f98470fe7
shahirsizan <shahir.sizan18@gmail.com>
2024-06-16 17:04:42 +0600

    Added more controller code for videos and routes for comments

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -113,23 +113,25 @@ export const sub = async (req, res, next) => {
 };
 
 export const getByTag = async (req, res, next) => {
-	// const tags = req.query.tags.split(",");
-	// try {
-	// 	const videos = await Video.find({ tags: { $in: tags } }).limit(20);
-	// 	res.status(200).json(videos);
-	// } catch (err) {
-	// 	next(err);
-	// }
+	// console.log(req);
+	const tags = req.query.tags.split(",");
+	// console.log(tags); //[ 'js', 'py', 'c' ]
+	try {
+		const videos = await Video.find({ tags: { $in: tags } }).limit(20);
+		res.status(200).json(videos);
+	} catch (err) {
+		next(err);
+	}
 };
 
 export const search = async (req, res, next) => {
-	//   const query = req.query.q;
-	//   try {
-	//     const videos = await Video.find({
-	//       title: { $regex: query, $options: "i" },
-	//     }).limit(40);
-	//     res.status(200).json(videos);
-	//   } catch (err) {
-	//     next(err);
-	//   }
+	const query = req.query.q;
+	try {
+		const videos = await Video.find({
+			title: { $regex: query, $options: "i" },
+		}).limit(40);
+		res.status(200).json(videos);
+	} catch (err) {
+		next(err);
+	}
 };
--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,7 +1,12 @@
 import express from "express";
 import {} from "../controllers/comment.js";
 // don't write "../controllers/user", this will render `ERR_MODULE_NOT_FOUND`
-
+// import { addComment, deleteComment, getComments } from "../controllers/comment.js";
+import { verifyToken } from "../verifyToken.js";
 const router = express.Router();
+
+router.post("/", verifyToken, addComment);
+router.delete("/:id", verifyToken, deleteComment);
+router.get("/:videoId", getComments);
 
 export default router;
